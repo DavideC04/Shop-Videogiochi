0a039a2a39ff6c753a408f980fbbfce7653c2705
Davide Cangiano <cangiano.0497@gmail.com>
2023-10-04 10:14:33 +0200

    Initial commit

---
+++ src/main/java/org/exercise/ShopVideogiochi/ShopVideogiochiApplication.java
@@ -0,0 +1,13 @@
+package org.exercise.ShopVideogiochi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopVideogiochiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopVideogiochiApplication.class, args);
+	}
+
+}
