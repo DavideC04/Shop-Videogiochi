df763f376f19bb53fe8c99741f5336ed459b8aa6
Ales <alocatelli798@gmail.com>
2023-10-05 11:56:49 +0200

    aggiunto slides alla homepage.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,3 @@
 --esempio - da modificare e aggiungere altri con stesso formato immagine
 INSERT INTO games (photo, title, editor, description, price) VALUES ('https://preview.redd.it/hbzsvtal3ca71.jpg?width=1080&crop=smart&auto=webp&s=96167222df2ac17a99da08dd82b54a05ff674040','GTA VI', 'Rockstar Games', 'Description test asdajdoauwdhiauhdsudhakdakd', 80);
+
