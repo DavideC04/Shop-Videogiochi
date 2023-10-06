f9afbfc41625c2adfbe729b59f41a97d403f3451
FrancescoGuggino <landongu@gmail.com>
2023-10-06 11:37:57 +0200

    User model id

--- src/main/java/org/exercise/ShopVideogiochi/model/User.java
+++ src/main/java/org/exercise/ShopVideogiochi/model/User.java
@@ -1,6 +1,9 @@
 package org.exercise.ShopVideogiochi.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
@@ -8,6 +11,8 @@ import java.time.LocalDate;
 @Entity
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String lastName;
