caef8bcc9c08aa28e6f9a3e5da3682d2be6b03ee
Ales <alocatelli798@gmail.com>
2023-10-11 09:11:54 +0200

    creato query custom

--- src/main/java/org/exercise/ShopVideogiochi/repository/PurchaseRepository.java
+++ src/main/java/org/exercise/ShopVideogiochi/repository/PurchaseRepository.java
@@ -2,6 +2,13 @@ package org.exercise.ShopVideogiochi.repository;
 
 import org.exercise.ShopVideogiochi.model.Purchase;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PurchaseRepository extends JpaRepository<Purchase, Integer> {
+
+    @Query("SELECT p FROM Purchase p WHERE MONTH(p.dateTime) = MONTH(CURRENT_DATE()) AND  YEAR (p.dateTime) = YEAR (CURRENT_DATE())")
+    List<Purchase> findPurchasesCurrMonthAndYear();
+
 }
