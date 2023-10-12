b986c4d7001661fc691422e1b14ff72f4fa15b4b
Davide Cangiano <cangiano.0497@gmail.com>
2023-10-12 11:37:40 +0200

    Created Role model

---
+++ src/main/java/org/exercise/ShopVideogiochi/model/Role.java
@@ -0,0 +1,28 @@
+package org.exercise.ShopVideogiochi.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Role {
+    @Id
+    private Integer id;
+    private String name;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/main/java/org/exercise/ShopVideogiochi/model/User.java
+++ src/main/java/org/exercise/ShopVideogiochi/model/User.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.NotBlank;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 public class User {
@@ -28,6 +29,9 @@ public class User {
     private String address;
     @OneToMany(mappedBy = "user")
     private List<Purchase> purchases;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Set<Role> roles;
 
     public User() {
 
